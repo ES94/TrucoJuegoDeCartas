ddd2878a7a0ae27aff803e72b64bd08c6c5728ea
ES94 <e.stradella94@outlook.com>
2017-04-12 19:20:32 -0300

    Commit inicial

---
+++ TrucoJuegoDeCartas/TrucoJuegoDeCartas.Entidades/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrucoJuegoDeCartas.Entidades
+{
+    public class Class1
+    {
+    }
+}
